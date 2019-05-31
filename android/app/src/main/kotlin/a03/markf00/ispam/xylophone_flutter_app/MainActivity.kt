c8a17e113390ed9436f1905ae8052ed50d0787cb
iSpammer <ispamossama@gmail.com>
2019-05-31 14:07:25 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/a03/markf00/ispam/xylophone_flutter_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package a03.markf00.ispam.xylophone_flutter_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNum) {
+    final player = AudioCache();
+    player.play("note$noteNum.wav");
+  }
+
+  Expanded buildKey(int noteNum, Color colorType) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(1);
+        },
+        color: colorType,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.deepPurple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
